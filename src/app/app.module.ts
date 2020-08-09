1f45bb9fd96f70fb80e5242309a0b8c5af64b180
DMarius1 <marius.dobrica17@gmail.com>
2020-08-09 14:22:50 +0300

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RouterModule, Routes, ExtraOptions } from '@angular/router';
+import { ProfileComponent } from './profile/profile.component';
+import { ProfileModule } from './profile/profile.module';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
 
+const routes: Routes = [
+  {
+    path:'',
+    component: ProfileComponent
+  }
+];
+
+const config: ExtraOptions = {
+  useHash: true,
+};
+ 
 @NgModule({
-  declarations: [
+  declarations:[
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot(routes,config),
+    ProfileModule,
+    HttpClientModule
   ],
+
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
